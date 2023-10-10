ca6b965f8d77ca5e886e83e00a8c8ddf4730058e
kairbayeva <dkairbaeva03@gmail.com>
2023-10-10 18:22:50 +0600

    add 1 module tasks

---
+++ module1/task2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Internal;
+
+public class task2
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter i:");
+        string stri = Console.ReadLine();
+        int i = 0;
+        i = Convert.ToInt32(stri);
+        Console.Write("Enter j:");
+        string strj = Console.ReadLine();
+        int j = 0;
+        j = Convert.ToInt32(strj);
+        int Sum = i + j;
+        Console.WriteLine("Sum = " + Sum);
+
+        Console.ReadLine();
+    }
+}
