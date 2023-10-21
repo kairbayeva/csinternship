64b07c15414b02eec9168a2408e93cd49abc568a
kairbayeva <dkairbaeva03@gmail.com>
2023-10-21 16:32:23 +0600

    add module3

---
+++ module3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppModule03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = "123 45 674 92 324 7893 12";
+            string[] words = input.Split(' ');
+
+            int sum = 0;
+
+            foreach (string i in words)
+            {
+                if (i.EndsWith("3") || i.EndsWith("4"))
+                {
+                    if (int.TryParse(i, out int number))
+                    {
+                        sum += number;
+                    }
+                }
+            }
+
+            Console.WriteLine("Сумма чисел, оканчивающихся на 3 или 4: " + sum);
+            Console.ReadLine();
+        }
+    }
+}
