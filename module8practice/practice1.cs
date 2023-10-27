92909d3fa4b8443dce2e25eb5363684e1308e3c4
kairbayeva <dkairbaeva03@gmail.com>
2023-10-27 18:26:15 +0600

    practice module8

---
+++ module8practice/practice1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace module8practice
+{
+    public class practice1
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the start of the range: ");
+            int start = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the end of the range: ");
+            int end = int.Parse(Console.ReadLine());
+
+            RangeOfArray customArrRange = new RangeOfArray();
+            int[] result = customArrRange.GetRange(start, end);
+
+            foreach (int item in result)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class RangeOfArray
+    {
+        private int[] arr = new int[5] { 9, 12, 13, 14, 15 };
+
+        public int[] GetRange(int start, int end)
+        {
+            if (start > end)
+            {
+                Console.WriteLine("Error: Start is greater than end.");
+                return new int[0];
+            }
+
+            int length = end - start + 1;
+            int[] result = new int[length];
+
+            for (int i = 0, j = start; i < length; i++, j++)
+            {
+                result[i] = arr[j];
+            }
+
+            return result;
+        }
+    }
+}
+
+
+
---
+++ module8practice/practice3.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace module8practice
+{
+     class practice3
+	{
+            static void Main(string[] args)
+            {
+               
+                double[] monthlySales = { 100, 120, 135, 150, 165 };
+                int numMonths = monthlySales.Length;
+                double sumX = 0, sumY = 0, sumXY = 0, sumX2 = 0;
+
+                for (int i = 0; i < numMonths; i++)
+                {
+                    double month = i + 1;
+                    double sales = monthlySales[i];
+
+                    sumX += month;
+                    sumY += sales;
+                    sumXY += month * sales;
+                    sumX2 += month * month;
+                }
+                double a = (numMonths * sumXY - sumX * sumY) / (numMonths * sumX2 - sumX * sumX);
+                double b = (sumY - a * sumX) / numMonths;
+
+                double nextMonth = a * (numMonths + 1) + b;
+                double monthAfterNext = a * (numMonths + 2) + b;
+                double thirdMonth = a * (numMonths + 3) + b;
+
+                Console.WriteLine("Прогноз продаж:");
+                Console.WriteLine($"Следующий месяц: {nextMonth}");
+                Console.WriteLine($"Месяц через два месяца: {monthAfterNext}");
+                Console.WriteLine($"Месяц через три месяца: {thirdMonth}");
+            }
+        }
+
+    }
+
+
+
