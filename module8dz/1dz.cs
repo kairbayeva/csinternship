57a2257b94d01908a99c13a20f6f227edbee0aac
kairbayeva <dkairbaeva03@gmail.com>
2023-10-27 20:48:21 +0600

    homework module8

---
+++ module8dz/1dz.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+class SquaredArr
+{
+    private double[] arr;
+
+    public SquaredArr(int size)
+    {
+        arr = new double[size];
+    }
+
+    public double this[int index]
+    {
+        get
+        {
+            if (index >= 0 && index < arr.Length)
+            {
+                return arr[index];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+        }
+        set
+        {
+            if (index >= 0 && index < arr.Length)
+            {
+                arr[index] = value * value;
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Index is out of range.");
+            }
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        SquaredArr squaredarr = new SquaredArr(5);
+        squaredarr[0] = 1.0;
+        squaredarr[1] = 2.0;
+        Console.WriteLine(squaredarr[0]);
+        Console.WriteLine(squaredarr[1]);
+    }
+}
+
---
+++ module8dz/2dz.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+class programm
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Площадь (м2): ");
+        double area = double.Parse(Console.ReadLine());
+        Console.Write("Количество жильцов: ");
+        int residents = int.Parse(Console.ReadLine());
+        Console.Write("Сезон (осень/зима/весна/лето): ");
+        string season = Console.ReadLine().ToLower();
+        Console.Write("Льготы (да/нет): ");
+        bool hasDiscount = Console.ReadLine().ToLower() == "да";
+
+        double heatingRate = (season == "осень" || season == "зима") ? 12 : 10;
+        double waterRate = 5;
+        double gasRate = 7;
+        double repairRate = 15;
+
+        double heatingPayment = area * heatingRate;
+        double waterPayment = residents * waterRate;
+        double gasPayment = residents * gasRate;
+        double repairPayment = area * repairRate;
+
+        double totalPayment = heatingPayment + waterPayment + gasPayment + repairPayment;
+
+        double discount = hasDiscount ? 0.5 : 0.3;
+        double discountedHeating = heatingPayment * discount;
+        double discountedWater = waterPayment * discount;
+        double discountedGas = gasPayment * discount;
+        double discountedRepair = repairPayment * discount;
+
+        Console.WriteLine("\nТаблица сумм коммунальных платежей:");
+        Console.WriteLine("Вид платежа\tНачислено\tЛьготная скидка\tИтого");
+        Console.WriteLine($"Отопление\t{heatingPayment}\t{discountedHeating}\t{heatingPayment - discountedHeating}");
+        Console.WriteLine($"Вода\t\t{waterPayment}\t{discountedWater}\t{waterPayment - discountedWater}");
+        Console.WriteLine($"Газ\t\t{gasPayment}\t{discountedGas}\t{gasPayment - discountedGas}");
+        Console.WriteLine($"Текущий ремонт\t{repairPayment}\t{discountedRepair}\t{repairPayment - discountedRepair}");
+        Console.WriteLine($"Итого\t\t{totalPayment}\t{discount * (heatingPayment + waterPayment + gasPayment + repairPayment)}\t{totalPayment - discount * (heatingPayment + waterPayment + gasPayment + repairPayment)}");
+    }
+}
+
