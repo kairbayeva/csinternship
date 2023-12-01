8040877bab61abd02bd5acb4bc7080d7fa85cc29
kairbayeva <122846694+kairbayeva@users.noreply.github.com>
2023-12-01 21:37:12 +0600

    Update Program.cs

--- module14/Program.cs
+++ module14/Program.cs
@@ -1,3 +1,118 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
+class Karta
+{
+    public string Mast { get; set; }
+    public string Tip { get; set; }
+
+    public Karta(string mast, string tip)
+    {
+        Mast = mast;
+        Tip = tip;
+    }
+}
+
+class Player
+{
+    public string Name { get; }
+    public List<Karta> Cards { get; } = new List<Karta>();
+
+    public Player(string name)
+    {
+        Name = name;
+    }
+
+    public void DisplayCards()
+    {
+        Console.WriteLine($"{Name}'s cards: {string.Join(", ", Cards.Select(karta => $"{karta.Tip} {karta.Mast}"))}");
+    }
+}
+
+class Game
+{
+    private List<Player> players = new List<Player>();
+    private List<Karta> deck = new List<Karta>();
+
+    public Game(List<string> playerNames)
+    {
+        players = playerNames.Select(name => new Player(name)).ToList();
+        InitializeDeck();
+        ShuffleDeck();
+        DealCards();
+    }
+
+    private void InitializeDeck()
+    {
+        string[] masts = { "Hearts", "Diamonds", "Clubs", "Spades" };
+        string[] tips = { "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace" };
+
+        foreach (var mast in masts)
+        {
+            foreach (var tip in tips)
+            {
+                deck.Add(new Karta(mast, tip));
+            }
+        }
+    }
+
+    private void ShuffleDeck()
+    {
+        Random random = new Random();
+        deck = deck.OrderBy(x => random.Next()).ToList();
+    }
+
+    private void DealCards()
+    {
+        int numberOfPlayers = players.Count;
+        int cardsPerPlayer = deck.Count / numberOfPlayers;
+
+        for (int i = 0; i < numberOfPlayers; i++)
+        {
+            players[i].Cards.AddRange(deck.GetRange(i * cardsPerPlayer, cardsPerPlayer));
+        }
+    }
+
+    public void Play()
+    {
+        while (players.All(player => player.Cards.Count > 0))
+        {
+            List<Karta> cardsInPlay = players.Select(player => player.Cards[0]).ToList();
+            Console.WriteLine($"Cards in play: {string.Join(", ", cardsInPlay.Select(karta => $"{karta.Tip} {karta.Mast}"))}");
+
+            int maxCardIndex = cardsInPlay.IndexOf(cardsInPlay.Max());
+            Player roundWinner = players[maxCardIndex];
+
+            Console.WriteLine($"{roundWinner.Name} wins the round!");
+
+            players.ForEach(player => player.Cards.RemoveAt(0));
+            roundWinner.Cards.AddRange(cardsInPlay);
+
+            foreach (var player in players)
+            {
+                player.DisplayCards();
+            }
+            Console.WriteLine();
+        }
+
+        Player gameWinner = players.Single(player => player.Cards.Count == 36);
+        Console.WriteLine($"{gameWinner.Name} wins the game!");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        List<string> playerNames = new List<string> { "Player1", "Player2" };
+        Game game = new Game(playerNames);
+
+        foreach (var player in game.Players)
+        {
+            player.DisplayCards();
+        }
+
+        game.Play();
+    }
+}
